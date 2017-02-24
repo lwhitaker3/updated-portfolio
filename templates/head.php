a15cdf86b5b9595592bc8d061df0c1bdc834530d
Louise Whitaker <l.whitaker@umiami.edu>
2017-02-23 21:09:06 -0500

    Add OG Tags

--- templates/head.php
+++ templates/head.php
@@ -8,3 +8,9 @@
 <link rel="stylesheet" href="/css/main.css">
 <link rel="shortcut icon" type="image/x-icon" href="/favicon/favicon.ico">
 <script src="/js/vendor/modernizr-2.6.2.min.js"></script>
+
+<meta property="og:site_name" content="Louise Whitaker's Portfolio"/>
+<meta property='og:image' content='/img/backgrounds/preview.jpg'/>
+<meta property='og:description' content='HI THERE! Let me introduce myself...My name is Louise! I am a user experience designer from Seattle. I am currently working on my MFA in Interactive Media at the University of Miami with a specialization in UX/UI design and will be graduating in May 2017.'/>
+<meta property='og:url' content='http://www.louisewhitaker.com'/>
+<meta property="og:type" content="website"/>
