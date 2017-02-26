063dee12796d27bd2ee67c8371d4c3a1ba12438d
Louise Whitaker <l.whitaker@umiami.edu>
2017-02-25 22:33:42 -0500

    Resume Fixes

--- resume.php
+++ resume.php
@@ -31,7 +31,7 @@
                 </div>
               </div>
               <p class="adjectives">Creative Thinker <span>&#8226;</span> Problem Solver <span>&#8226;</span> UX Designer</p>
-              <p data-aos="fade" data-aos-once="true" class="larger">I am a grad student at the University of Miami working towards my MFA in Interactive Media with a focus in UX/UI design. Through my experiences, I have learned the importance of good customer service and creating interactions that are pleasant for the customer. Technology should work the same. Users should enjoy interacting with products and understand how they work intuitively. I work iteratively with an user-centered design approach to discover strengths and weaknesses of different ideas. My strengths include developing wireframes and creating rapid prototypes to explore how the product should function.  Upon graduation this May, I am seeking to join a UX team committed to developing innovative and meaningful projects that are user-friendly.</p>
+              <p data-aos="fade" data-aos-once="true" class="larger">I am a grad student at the University of Miami working towards my MFA in Interactive Media with a focus in UX/UI design. Through my experiences, I have learned the importance of good customer service and creating interactions that are pleasant for the customer. Technology should work the same. Users should enjoy interacting with products and understand how they work intuitively. I work iteratively with a user-centered design approach to uncover the strengths and weaknesses of different ideas. My strengths include developing wireframes and creating rapid prototypes to explore how the product should function. Upon graduation this May, I am seeking to join a UX team committed to developing innovative and meaningful projects that are user-friendly.</p>
             </div>
           </div>
           <div data-aos="fade-up" data-aos-once="true" class="row">
@@ -80,7 +80,57 @@
 
       <div class="container-fluid space">
         <div class="container flyin-container">
-          <h2 data-aos="fade" data-aos-once="true" class="center margin-top-50">Timeline</h2>
+          <h2 data-aos="fade" data-aos-once="true" class="center margin-top-50">Education</h2>
+          <div data-aos="fade" data-aos-once="true" class="row">
+            <div class="col-xs-offset-5 col-xs-2">
+              <div class="lineBreak color"></div>
+            </div>
+          </div>
+
+          <div class="resume-section-wrapper">
+            <div class="row">
+              <div class="col-sm-12 col-lg-10">
+                <div class="row">
+
+                  <div class="hidden-xs col-sm-3 text-center">
+                    <img data-aos="fade" data-aos-once="true" class="resumeImg" alt="Education Icon" src="img/icons/school.png">
+                  </div>
+                  <div data-aos="fade-left" data-aos-once="true" class="col-sm-9 resume-entry">
+                    <p class="resume-date">August 2015 - Present<span> Coral Gables, FL</span></p>
+                    <p class="resume-position">Master of Fine Arts - Interactive Media</p>
+                    <p class="resume-title"><span>University of Miami</span></p>
+                    <ul class="resume-description">
+                      <li><span><span class="bold">Expected Graduation:</span> May 2017</span></li>
+                      <li><span><span class="bold">Current GPA:</span> 4.0</span></li>
+                      <li><span><span class="bold">Course Work:</span> Creative Code, Building Interfaces, Data Visualization, Designing Interactivity, 3d Design, UI Design, UX Research, PHP</span></li>
+                    </ul>
+                  </div>
+
+                  <div class="hidden-xs col-sm-3 text-center">
+                    <img data-aos="fade" data-aos-once="true" class="resumeImg" alt="Education Icon" src="img/icons/grad.png">
+                  </div>
+                  <div data-aos="fade-left" data-aos-once="true" class="col-sm-9 resume-entry">
+                    <p class="resume-date">August 2011 - May 2015<span> Coral Gables, FL</span></p>
+      							<p class="resume-position">BSC - Visual Journalism and Political Science</p>
+                    <p class="resume-title">University of Miami</p>
+      							<ul class="resume-description">
+      								<li><span><span class="bold">Cumulative GPA:</span> 3.78 Magna Cum Laude</li></span>
+      								<li><span><span class="bold">Participated in:</span> Honor Council, Kids and Culture, Alpha Phi Omega</li></span>
+      								<li><span><span class="bold">Course Work:</span> Public Speaking, Programming for Interactivity, Statistics for Political Science, Infographics, Web Design, Computer Programming I, Interaction Design</li></span>
+      							</ul>
+                  </div>
+
+                  <div class="hidden-xs vertical-line"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid space">
+        <div class="container flyin-container">
+          <h2 data-aos="fade" data-aos-once="true" class="center margin-top-50">Work Experience</h2>
           <div data-aos="fade" data-aos-once="true" class="row">
             <div class="col-xs-offset-5 col-xs-2">
               <div class="lineBreak color"></div>
@@ -100,13 +150,15 @@
                     <p class="resume-position">Experience Design Intern</p>
                     <p class="resume-title">SapientNitro</p>
                     <ul class="resume-description">
-                      <li><span>Worked with project teams to develop creative web solutions for clients.</span></li>
-                      <li><span>Created wireframes and built prototypes to demonstrate proof of concepts.</span></li>
-                      <li><span>Conducted competitive analyses to gain a better understanding of the competition.</span></li>
-                      <li><span>Researched a variety of topics to provide advice to clients.</span></li>
+                      <li><span>Worked with project teams to develop creative solutions for clients.</span></li>
+                      <li><span>Participated in client meetings to get feedback for further iterations.</span></li>
+                      <li><span>Created design docs such as  task flows, site maps, and wireframes to keep projects organized.</span></li>
+                      <li><span>Developed prototypes with Axure to demonstrate proof of concept and hand off to development teams for implementation.</span></li>
+                      <li><span>Designed and developed the website for the Summer 2016 intern project using Axure and HTML/CSS/JavaScript. <a target="_blank" href="/portfolio/good_lemon.php">See Project Here.</a></span></li>
+                      <li><span>Clients: Carnival Cruise Lines, Universal Studios, Seminole Gaming.</span></li>
+
                     </ul>
                   </div>
-
 
                   <div class="hidden-xs col-sm-3 text-center">
                     <img data-aos="fade" data-aos-once="true" class="resumeImg" alt="UX Research Icon" src="img/icons/ux-23.png">
@@ -116,10 +168,11 @@
                     <p class="resume-position">UX Research Assistant</p>
                     <p class="resume-title">School of Communication</p>
                     <ul class="resume-description">
-                      <li><span>Carried out UX research to build an application for exposure records to prevent cancer among firefighters. Used focus groups, card sorts, and competitive analyses to make sure the app met firefighters needs. Conducted usability testing with firefighters to investigate potential issues with the system.</span></li>
+                      <li><span>Worked on application for exposure records designed to look at trends to prevent cancer among firefighters.</span></li>
+                      <li><span>Carried out front-end research such as competitive analysis, focus groups, and card sorts to develop requirements for project.</span></li>
+                      <li><span>Conducted heuristic evaluations on all iterations and usability testing on production code to ensure application met firefighters’ needs and investigate potential issues with the system.</span></li>
                       <li><span>Completed expert reviews, offered recommendations for redesigns based on findings, and helped restructure the information architecture for a clinical trial search site.</span></li>
                       <li><span>Conducted heuristic reviews of a program website and offered solutions for redesign to improve the user experience and content of the website.</span></li>
-                      <li><span>Developed test plans and task scenarios for usability testing.</span></li>
                     </ul>
                   </div>
 
@@ -131,40 +184,38 @@
                     <p class="resume-position">Web Designer</p>
                     <p class="resume-title">Department of Computational Science</span>
                     <ul class="resume-description">
-                      <li><span>Created microsites for the department.</span></li>
-                      <li><span>Developed a stronger information architecture for existing pages.</span></li>
-                      <li><span>Coded new designs in HTML, CSS, and JavaScript.</span></li>
-                      <li><span>Provided recommendations for improved designs by crafting and iteratively refining wireframes based on user feedback.</span></li>
+                      <li><span>Developed stronger information architecture and visual hierarchy for existing pages.</span></li>
+                      <li><span>Provided wireframes for review and refined design based on stakeholder feedback. </span></li>
+                      <li><span>Designed mock ups of visual design for sites.</span></li>
+                      <li><span>Coded new designs in HTML/CSS/JavaScript. <a target="_blank" href="/portfolio/ccs.php">See Projects Here.</a></span></li></span></li>
                     </ul>
                   </div>
-
-                  <div class="hidden-xs col-sm-3 text-center">
-                    <img data-aos="fade" data-aos-once="true" class="resumeImg" alt="Education Icon" src="img/icons/school.png">
-                  </div>
-                  <div data-aos="fade-left" data-aos-once="true" class="col-sm-9 resume-entry">
-                    <p class="resume-date">August 2015 - Present<span> Coral Gables, FL</span></p>
-                    <p class="resume-position">Master of Fine Arts - Interactive Media</p>
-                    <p class="resume-title"><span>University of Miami</span></p>
-                    <ul class="resume-description">
-                      <li><span><span class="bold">Expected Graduation:</span> May 2017</span></li>
-                      <li><span><span class="bold">Current GPA:</span> 4.0</span></li>
-                      <li><span><span class="bold">Course Work:</span> Creative Code, Building Interfaces, Data Visualization, Designing Interactivity, 3d Design, UI Design, UX Research, PHP</span></li>
-                    </ul>
-                  </div>
-
-
 
                   <div class="hidden-xs col-sm-3 text-center">
                     <img data-aos="fade" data-aos-once="true" class="resumeImg" alt="Code Icon" src="img/icons/code.png">
                   </div>
                   <div data-aos="fade-left" data-aos-once="true" class="col-sm-9 resume-entry">
                     <p class="resume-date">August 2014 - May 2015<span> Coral Gables, FL</span></p>
-      							<p class="resume-position">Website Editor</p>
+      							<p class="resume-position">Web Editor</p>
       							<p class="resume-title">Ibis Yearbook</span>
       							<ul class="resume-description">
-      								<li><span>Made regular updates to the WordPress site and posted photos of the week.</span></li>
-      								<li><span>Reorganized the structure of the pages to improve management of site.</span></li>
+      								<li><span>Utilized Wordpress to keep site updated and edited photos for web.</span></li>
+      								<li><span>Reorganized structure of pages to improve management of site.</span></li>
       							</ul>
+                  </div>
+
+                  <div class="hidden-xs col-sm-3 text-center">
+                    <img data-aos="fade" data-aos-once="true" class="resumeImg" alt="Code Icon" src="img/icons/code.png">
+                  </div>
+                  <div data-aos="fade-left" data-aos-once="true" class="col-sm-9 resume-entry">
+                    <p class="resume-date">Jan 2014 - May 2015<span> Coral Gables, FL</span></p>
+                    <p class="resume-position">Web Developer</p>
+                    <p class="resume-title">Net-Zero Water Project</span>
+                    <ul class="resume-description">
+                      <li><span>Managed Net-Zero Water Project HTML files to keep site up-to-date.</span></li>
+                      <li><span>Developed website for Net Zero Water Station in HTML/CSS.</span></li>
+                      <li><span>Worked with engineers to make site clear to non-engineers.</span></li>
+                    </ul>
                   </div>
 
 
@@ -196,20 +247,6 @@
       							</ul>
                   </div>
 
-                  <div class="hidden-xs col-sm-3 text-center">
-                    <img data-aos="fade" data-aos-once="true" class="resumeImg" alt="Education Icon" src="img/icons/grad.png">
-                  </div>
-                  <div data-aos="fade-left" data-aos-once="true" class="col-sm-9 resume-entry">
-                    <p class="resume-date">August 2011 - May 2015<span> Coral Gables, FL</span></p>
-      							<p class="resume-position">BSC - Visual Journalism and Political Science</p>
-                    <p class="resume-title">University of Miami</p>
-      							<ul class="resume-description">
-      								<li><span><span class="bold">Cumulative GPA:</span> 3.78 Magna Cum Laude</li></span>
-      								<li><span><span class="bold">Participated in:</span> Honor Council, Kids and Culture, Alpha Phi Omega</li></span>
-      								<li><span><span class="bold">Course Work:</span> Public Speaking, Programming for Interactivity, Statistics for Political Science, Infographics, Web Design, Computer Programming I, Interaction Design</li></span>
-      							</ul>
-                  </div>
-
 
                   <div class="hidden-xs vertical-line"></div>
                 </div>
@@ -221,7 +258,7 @@
 
 
 
-      <div class="other-experience-container container-fluid space text-center">
+      <!-- <div class="other-experience-container container-fluid space text-center">
         <div class="container flyin-container">
           <h2 data-aos="fade" data-aos-once="true" class="white margin-top-50">Projects</h2>
           <div data-aos="fade" data-aos-once="true" class="row">
@@ -262,7 +299,7 @@
             </div>
           </div>
         </div>
-      </div>
+      </div> -->
 
 
 
