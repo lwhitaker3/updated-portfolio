f729c9bf6c1ab0734c41cfc3329dae997b9220db
Louise Whitaker <l.whitaker@umiami.edu>
2017-02-27 11:23:56 -0500

    Fixed Broken Links

--- templates/project_menu_variables.php
+++ templates/project_menu_variables.php
@@ -1,39 +1,37 @@
 <?php
-
-  $dirList = array_map('basename', glob('../portfolio/*.php'));
+  $dirList = array_map('basename', glob('../portfolio/*'));
   $uri = $_SERVER['REQUEST_URI'];
-  $uriPage = substr(strrchr($uri, "/"), 1); // Outputs: URI
+  $uriPage = stripExtension(substr(strrchr($uri, "/"), 1)); // Outputs: URI
   $prev;
   $next;
-
   function prettyPrint($uri){
-    $page_name = substr($uri, 0, strpos($uri, '.'));
+    $page_name = stripExtension($uri);
     $page_name_array = (explode('_',$page_name));
     foreach ($page_name_array as $word) {
       $pretty_name .= ucfirst($word." ");
     }
     return $pretty_name;
   }
-
-  foreach ($dirList as $key => $page) {
+  function stripExtension($name) {
+    return preg_replace("/\\.php/", "", $name);
+  }
+  foreach ($dirList as $key => $pageWithExtension) {
+    $page = stripExtension($pageWithExtension);
     if ($uriPage == $page){
       if ($key == 0){
-        $prev = $dirList[(sizeof($dirList)-1)];
-        $next = $dirList[$key+1];
-
+        $prev = stripExtension($dirList[(sizeof($dirList)-1)]);
+        $next = stripExtension($dirList[$key+1]);
       }
       else if ($key == (sizeof($dirList)-1)){
-        $next = $dirList[0];
-        $prev = $dirList[$key-1];
+        $next = stripExtension($dirList[0]);
+        $prev = stripExtension($dirList[$key-1]);
       }
-
       else {
-        $prev = $dirList[$key-1];
-        $next = $dirList[$key+1];
+        $prev = stripExtension($dirList[$key-1]);
+        $next = stripExtension($dirList[$key+1]);
       }
       $next_name = prettyPrint($next);
       $prev_name = prettyPrint($prev);
     }
   }
-
 ?>
